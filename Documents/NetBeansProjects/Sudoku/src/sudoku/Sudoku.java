e047c1e5254e9dbef4eaba7ba90393841de43983
Jakub Typiński <typiak1995@poczta.onet.pl>
2018-02-28 18:49:28 +0100

    goal function added

--- Documents/NetBeansProjects/Sudoku/src/sudoku/Sudoku.java
+++ Documents/NetBeansProjects/Sudoku/src/sudoku/Sudoku.java
@@ -5,17 +5,57 @@
  */
 package sudoku;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
+import javax.swing.SpringLayout;
+
 /**
  *
  * @author Jarek
  */
-public class Sudoku {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Sudoku extends JFrame {
+    private final GamePanel gamePanel;
+    private final OptionsPanel optionsPanel;
+    
+    public Sudoku () {
+        Dimension ScreenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setPreferredSize( new Dimension(1000,550) );
+        
+        GamePanel gamePanel = new GamePanel();
+        this.gamePanel = gamePanel;
+        
+        OptionsPanel optionsPanel = new OptionsPanel();
+        this.optionsPanel = optionsPanel;
+        
+        OptionsBar optionsBar = new OptionsBar();
+        
+        SpringLayout springLayout = new SpringLayout();
+        setLayout(springLayout);
+        add(gamePanel);
+        add(optionsPanel);
+        setJMenuBar(optionsBar);
+        
+        
+        springLayout.putConstraint(SpringLayout.WEST, gamePanel, 10, SpringLayout.WEST, this.getContentPane());
+        springLayout.putConstraint(SpringLayout.NORTH, gamePanel, 10, SpringLayout.NORTH, this.getContentPane());
+        
+        springLayout.putConstraint(SpringLayout.WEST, optionsPanel, 10, SpringLayout.EAST, gamePanel);
+        springLayout.putConstraint(SpringLayout.NORTH, optionsPanel, 10, SpringLayout.NORTH, this.getContentPane());
+        
+        pack();
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater( new Runnable (){
+            @Override
+            public void run() {
+                Sudoku sudoku = new Sudoku();
+                sudoku.setVisible(true);
+            }        
+        });
     }
     
 }
